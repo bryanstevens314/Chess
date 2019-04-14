a3f11b7a7972a993479a7818d25ecc3a32579522
Bryan <bryan_stevens314@yahoo.com>
2019-04-13 21:18:48 -0700

    Update: helper files for pieces and initial board

---
+++ index/main.js
@@ -0,0 +1,14 @@
+const main = document.getElementById('main');
+import pieces from './pieces';
+import initial_Board from './board';
+
+window.onload = () => {
+  const board = document.createElement('div');
+  for (let i = 8; i >= 1; i--) {
+    const row = document.createElement('div');
+    for (let i = 8; i >= 1; i--) {
+      const space = document.createElement('div');
+      row.appendChild()
+    }
+  }
+}
